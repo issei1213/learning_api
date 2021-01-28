53bd107d510539fb9c87f65ecbdf9e4b6f840574
issei1213 <lord_0bf38w16110335h@yahoo.co.jp>
2021-01-28 20:47:14 +0900

    part #7

--- basic-rest-api/app/app.js
+++ basic-rest-api/app/app.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const app = express();
+const sqlite3 = require("sqlite3");
+const path = require("path")
 
-app.get("/api/v1/hello", (req, res) => {
-  res.json({ message: "Hello, World!" });
+const dbPath = "app/db/database.sqlite3";
+
+app.use(express.static(path.join(__dirname, "public")))
+
+// Get all users
+app.get("/api/v1/users", (req, res) => {
+  // Conncet database
+  const db = new sqlite3.Database(dbPath);
+
+  db.all("SELECT * FROM users", (err, rows) => {
+    res.json(rows);
+  })
+
+  db.close()
+});
+
+// Get a user
+app.get("/api/v1/users/:id", (req, res) => {
+  // Conncet database
+  const db = new sqlite3.Database(dbPath);
+  const id = req.params.id
+
+
+  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+    res.json(row);
+  })
+
+  db.close()
+});
+
+// Search users matching keyword
+app.get("/api/v1/search", (req, res) => {
+  // Conncet database
+  const db = new sqlite3.Database(dbPath);
+  const keyword = req.query.q
+
+  db.all(`SELECT * FROM users where name LIKE "%${keyword}%"`, (err, rows) => {
+    res.json(rows);
+  })
+
+  db.close()
 });
 
 const port = process.env.PORT || 3000;
